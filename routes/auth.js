050c2f5d0032807f3502dd7a2510d713c8e14078
Mahedere <mahederetebebe98@gmail.com>
2024-10-17 09:16:36 -1200

    formatting fixed

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,22 @@ const router = require("express").Router();
 const { check, validationResult } = require("express-validator");
 router.post(
   "/signup",
-  [check("email").isEmail(), check("password").isLength({ min: 6 })],
+  [
+    check("email", "Please provide a vaild email").isEmail(),
+    check(
+      "password",
+      "Please provide a password that is greater than 5 characters"
+    ).isLength({
+      min: 6,
+    }),
+  ],
   (req, res) => {
     const { password, email } = req.body;
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            errors:errors.array()
-        })
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
     }
     res.send("Validation Passed");
   }
