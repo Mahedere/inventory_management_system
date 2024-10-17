6e882809feb63bccaf96cd37cdabaeacdb42e0fd
Mahedere <mahederetebebe98@gmail.com>
2024-10-17 10:02:53 -1200

    validation feature completed

---
+++ db.js
@@ -0,0 +1,9 @@
+const users = [
+  {
+    email: "mahi@gmail.com",
+    password: "1234567890",
+  },
+];
+module.exports = {
+  users,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { check, validationResult } = require("express-validator");
+const { users } = require("../db");
 router.post(
   "/signup",
   [
@@ -13,10 +14,24 @@ router.post(
   ],
   (req, res) => {
     const { password, email } = req.body;
+    //Validates the Input
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({
         errors: errors.array(),
+      });
+    }
+    //validates if user doesn't already exist
+    let user = users.find((user) => {
+      return user.email === email;
+    });
+    if (user) {
+      res.status(400).json({
+        errors: [
+          {
+            msg: "This user already exists",
+          },
+        ],
       });
     }
     res.send("Validation Passed");
