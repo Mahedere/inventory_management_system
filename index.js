6acab2ecd4a979dbdde546ae9bcbc840d0a5fa56
Mahedere <mahederetebebe98@gmail.com>
2024-10-17 09:01:55 -1200

    express validator added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const auth = require("./routes/auth");
 const app = express();
+app.use(express.json());
+app.use("/auth", auth);
 app.get("/", (req, res) => {
   res.send("Hi i am working");
 });
---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const { check, validationResult } = require("express-validator");
+router.post(
+  "/signup",
+  [check("email").isEmail(), check("password").isLength({ min: 6 })],
+  (req, res) => {
+    const { password, email } = req.body;
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors:errors.array()
+        })
+    }
+    res.send("Validation Passed");
+  }
+);
+
+module.exports = router;
