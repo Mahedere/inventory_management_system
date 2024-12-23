329ca535ed0292e5afa6f169c5ab42acfe458dd4
Mahedere <mahederetebebe98@gmail.com>
2024-12-23 15:44:15 -0500

    attempt 6 vite config

--- frontend/inventory_management_system/vite.config.js
+++ frontend/inventory_management_system/vite.config.js
@@ -15,4 +15,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://inventory-backend-sepia.vercel.app/', 
+        changeOrigin: true,
+      },
+    },
+  },
 })
