03c4812e9387d23b01b5936273415104fb79fa60
Mahedere <mahederetebebe98@gmail.com>
2024-12-23 11:29:00 -0500

    attempt 3 cors error fixed

--- api/index.js
+++ api/index.js
@@ -14,10 +14,11 @@ connectDB();
 
 // Middleware
 app.use(
-  cors({ 
-    origin: "*",
-    // credentials: true
-   }))
+    cors({
+        origin: "https://inventory-management-system-kappa-silk.vercel.app",
+        methods: 'GET,POST,PUT,DELETE',
+        allowedHeaders: ['Content-Type', 'Authorization'],    // credentials: true
+    }))
 app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);
@@ -27,13 +28,13 @@ app.use('/api/sales', saleRoutes);
 app.use('/api/users', userRoutes);
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
 });
 
 const PORT = process.env.PORT || 5000;
 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
